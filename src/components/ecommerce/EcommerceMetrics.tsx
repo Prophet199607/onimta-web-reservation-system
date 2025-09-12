a84afcbaf351012c30fcca55c0156fa3f1ba514f
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-09-12 16:22:45 +0530

    Refactor EcommerceMetrics layout and update metrics

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -1,25 +1,50 @@
 import {
   TableIcon,
   GroupIcon,
-  ShootingStarIcon,
+  // ShootingStarIcon,
   CalenderIcon,
   BoxIcon,
   UserCircleIcon,
 } from "../../icons";
+import { IoBedOutline } from "react-icons/io5";
 
 export default function EcommerceMetrics() {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-4">
       {/* <!-- Metric Item Start --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
+        <div className="flex items-center justify-between">
+          {/* Icon */}
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
 
-        <div className="flex items-end justify-between mt-5">
-          <div>
+          {/* Text */}
+          <div className="text-right">
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Customers
+            </span>
+            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
+              100
+            </h4>
+          </div>
+        </div>
+      </div>
+
+      {/* <!-- Metric Item End --> */}
+
+      {/* <!-- Metric Item Start --> */}
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+        <div className="flex items-center justify-between">
+          {/* Icon */}
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <IoBedOutline className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
+
+          {/* Text */}
+          <div className="text-right">
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Room Types
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
               100
@@ -31,35 +56,18 @@ export default function EcommerceMetrics() {
 
       {/* <!-- Metric Item Start --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <ShootingStarIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
-
-        <div className="flex items-end justify-between mt-5">
-          <div>
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              Room Types
-            </span>
-            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              10
-            </h4>
+        <div className="flex items-center justify-between">
+          {/* Icon */}
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <CalenderIcon className="text-gray-800 size-6 dark:text-white/90" />
           </div>
-        </div>
-      </div>
-      {/* <!-- Metric Item End --> */}
-
-      {/* <!-- Metric Item Start --> */}
-      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <CalenderIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
-        <div className="flex items-end justify-between mt-5">
-          <div>
+          {/* Text */}
+          <div className="text-right">
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Event Types
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              10
+              100
             </h4>
           </div>
         </div>
@@ -68,17 +76,19 @@ export default function EcommerceMetrics() {
 
       {/* <!-- Metric Item Start --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <BoxIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
+        <div className="flex items-center justify-between">
+          {/* Icon */}
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <BoxIcon className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
 
-        <div className="flex items-end justify-between mt-5">
-          <div>
+          {/* Text */}
+          <div className="text-right">
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Packages
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              10
+              100
             </h4>
           </div>
         </div>
@@ -87,17 +97,19 @@ export default function EcommerceMetrics() {
 
       {/* <!-- Metric Item Start --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <TableIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
+        <div className="flex items-center justify-between">
+          {/* Icon */}
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <TableIcon className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
 
-        <div className="flex items-end justify-between mt-5">
-          <div>
+          {/* Text */}
+          <div className="text-right">
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Service Types
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              10
+              100
             </h4>
           </div>
         </div>
@@ -105,17 +117,19 @@ export default function EcommerceMetrics() {
 
       {/* <!-- Metric Item Start --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <UserCircleIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
+        <div className="flex items-center justify-between">
+          {/* Icon */}
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <UserCircleIcon className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
 
-        <div className="flex items-end justify-between mt-5">
-          <div>
+          {/* Text */}
+          <div className="text-right">
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Travel Agent
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              10
+              100
             </h4>
           </div>
         </div>
